bd9e3092adeed827404e8ba3ed4dafe0d74eff84
Camila Sousa <95882354+CamomilaCosta@users.noreply.github.com>
2023-01-19 19:59:08 -0300

    Update main.js

--- sinter/main.js
+++ sinter/main.js
@@ -11,3 +11,18 @@ const swiper = new Swiper('.swiper', {
     delay: 3800
   }
 })
+
+const swipertest = new Swiper('.swiper-testmonials', {
+  speed: 1000,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  loop: true,
+  autoplay: {
+    delay: 3800
+  }
+})
