7805e0c575a653c8ad3dfbc4bdc879e687adfb79
Camila Sousa <camilacdesousa2@gmail.com>
2023-02-17 10:16:44 -0300

    header responsivo

--- rj/header/main.js
+++ rj/header/main.js
@@ -1,17 +1,36 @@
 /*subitem menu */
 
 const menuCompany = document.querySelector('#menu-company')
+const forCompany = document.querySelector('#menu-company p')
 const menuSubitem = document.querySelector('#subitem')
 
-menuCompany.addEventListener('mouseover', function () {
-  menuSubitem.classList.add('hover')
-})
-menuSubitem.addEventListener('mouseover', function () {
-  this.classList.add('hover')
-})
-menuSubitem.addEventListener('mouseout', function () {
-  this.classList.remove('hover')
-})
-menuCompany.addEventListener('mouseout', function () {
-  menuSubitem.classList.remove('hover')
-})
+if (window.screen.width >= 992) {
+  menuCompany.addEventListener('mouseover', function () {
+    menuSubitem.classList.add('hover')
+  })
+  menuSubitem.addEventListener('mouseover', function () {
+    this.classList.add('hover')
+  })
+  menuSubitem.addEventListener('mouseout', function () {
+    this.classList.remove('hover')
+  })
+  menuCompany.addEventListener('mouseout', function () {
+    menuSubitem.classList.remove('hover')
+  })
+}
+
+if (window.screen.width < 991) {
+  forCompany.addEventListener('click', function () {
+    menuSubitem.classList.toggle('click')
+  })
+  const nav = document.querySelector('#content-menumobile')
+  const toggle = document.querySelectorAll('#menu-mobile span')
+  const iconx = document.querySelector('.icon-x')
+
+  for (const element of toggle) {
+    element.addEventListener('click', function () {
+      nav.classList.toggle('show')
+      iconx.classList.toggle('show')
+    })
+  }
+}
