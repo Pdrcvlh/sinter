5c0734f503f5c65b379b5930932df42f171abeb5
Camila Sousa <camilacdesousa2@gmail.com>
2023-02-17 14:32:03 -0300

    heade reusável em contato

---
+++ rj/header/header.js
@@ -0,0 +1,61 @@
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<div id="header">
+    <div class="container">
+      <!--Logo-->
+      <div id="logo-head">
+        <img src="../assets/logos/logo 5.png" alt="" />
+      </div>
+
+      <nav>
+        <!--Itens do menu-->
+        <div id="content-menumobile">
+          <ul id="menu">
+            <a href=""><li>Início</li></a>
+            <li id="menu-company">
+              <p>Para Empresas</p>
+              <!--Subitens do opção para empresas-->
+              <ul id="subitem">
+                <li><a href="">Banda Larga</a></li>
+                <li><a href="">Link Dedicado</a></li>
+              </ul>
+            </li>
+            <a href=""><li>Parceria</li></a>
+            <a href=""><li>Sobre Nós</li></a>
+            <a href=""><li>Contato</li></a>
+          </ul>
+          <div id="info-mobile">
+            <div class="location">
+              <span class="icon-location2"></span>
+              <p>Rio de Janeiro</p>
+            </div>
+            <div id="social-menu">
+              <p>Siga nossas redes sociais</p>
+              <div id="social-icon">
+                <a href=""><span class="icon-facebook"></span></a>
+                <a href=""><span class="icon-instagram"></span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--Menu mobile-->
+        <div id="menu-mobile">
+          <span class="icon-menu"></span>
+          <span class="icon-x"></span>
+        </div>
+      </nav>
+
+      <!--Botão de login-->
+      <div id="login">
+        <a href="">
+          <p>Área do Cliente</p>
+          <span class="icon-user"></span>
+        </a>
+        <!--login mobile-->
+      </div>
+    </div>
+  </div>`
+  }
+}
+
+customElements.define('my-header', MyHeader)
--- rj/header/main.js
+++ rj/header/main.js
@@ -1,10 +1,16 @@
 /*subitem menu */
-
+const header = document.querySelector('my-header')
 const menuCompany = document.querySelector('#menu-company')
 const forCompany = document.querySelector('#menu-company p')
+
 const menuSubitem = document.querySelector('#subitem')
 
+const nav = document.querySelector('#content-menumobile')
+const toggle = document.querySelectorAll('#menu-mobile span')
+const iconx = document.querySelector('.icon-x')
+
 if (window.screen.width >= 992) {
+  console.log(window.screen.width)
   menuCompany.addEventListener('mouseover', function () {
     menuSubitem.classList.add('hover')
   })
@@ -19,18 +25,17 @@ if (window.screen.width >= 992) {
   })
 }
 
-if (window.screen.width < 991) {
-  forCompany.addEventListener('click', function () {
-    menuSubitem.classList.toggle('click')
-  })
-  const nav = document.querySelector('#content-menumobile')
-  const toggle = document.querySelectorAll('#menu-mobile span')
-  const iconx = document.querySelector('.icon-x')
+console.log(window.width)
+forCompany.addEventListener('click', function () {
+  menuSubitem.classList.toggle('click')
+})
 
-  for (const element of toggle) {
-    element.addEventListener('click', function () {
-      nav.classList.toggle('show')
-      iconx.classList.toggle('show')
-    })
-  }
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+    iconx.classList.toggle('show')
+  })
+}
+
+if (window.screen.width < 991) {
 }
